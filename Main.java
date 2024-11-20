2c7ed9719d134691826469f4630a705a5c6d8481
capillanjohn <jccapillan28@gmail.com>
2024-11-20 20:53:56 +0800

    Output done

--- Main.java
+++ Main.java
@@ -1,1 +1,18 @@
+class Tasksheet113{
+    public static void main(String[] args) {
+        // Declaring Variables
+        char h = 'H';
+        short num = 3110;
+        byte zero = 0;
+        int one = 1;
+         float two = 2.0f;
+         boolean truth = true;
+         
+         String output = "" + h + num +  "w" + zero + "r"  + one +"d " + two + " " + truth;
+         
+         System.out.println(output);
+      
+    }
+
+}
 
